052487476a0e272e81ab8b3ed31b8d459025017c
cholakovge <cholakovge@gmail.com>
2020-02-01 15:18:24 +0200

    Streams, Files and Directories

---
+++ 3.Sets And Dictionaries Advanced - Exercise/SetsAndDictionaries_ex/Count_Symbols/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Count_Symbols
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = Console.ReadLine().ToCharArray();
+
+            //var set = new HashSet<char>();
+            var dict = new SortedDictionary<char, int>();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (!dict.ContainsKey(text[i]))
+                {
+                    dict.Add(text[i], 0);
+                }
+                dict[text[i]]++;
+            }
+
+            foreach (var @char in dict)
+            {
+                Console.WriteLine($"{@char.Key}: {@char.Value} time/s");
+            }
+        }
+    }
+}
---
+++ 4.Streams, Files And Directories - Lecture/Stream_And_Files/Test/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var reader = new StreamReader("input"))
+            {
+                int counter = 0;
+                string line = reader.ReadLine();
+
+                using (var writer = new StreamWriter("output"))
+                {
+                    while (reader.EndOfStream)
+                    {
+                        if (counter % 2 == 1)
+                        {
+                            writer.WriteLine(line);
+                        }
+                        counter++;
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+        }
+    }
+}
